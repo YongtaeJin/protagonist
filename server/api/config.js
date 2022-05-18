472ade62899f0d499e1338cc3aa8be502b63b850
YongTaeJin <freview@nate.com>
2022-05-18 17:31:55 +0900

    059_Json값 처리

--- server/api/config.js
+++ server/api/config.js
@@ -25,7 +25,7 @@ router.put('/', async (req,res) => {
 router.delete('/:cf_key', async (req,res) => {
     const result = await modelCall(configModel.removeConfig, req);
     res.json(result);
-}) ;
+});
 
 
 module.exports = router;
